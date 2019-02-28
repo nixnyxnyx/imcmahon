46d3ec1c77fa1caafe759e57ad6e2f742d293c49
nixnyxnyx <isaacmcmahon@cox.net>
2019-02-28 18:13:34 -0500

    f

--- loading.js
+++ loading.js
@@ -19,3 +19,29 @@ function startup() {
     $(".loading-screen").removeClass("loading-screen-on");
     setTimeout(function() {$(".loading-screen").css("display", "none") }, 500);
 }
+
+//https://dev.to/phillt/inherit-the-width-of-the-parent-element-when-position-fixed-is-applied
+
+ function toggleFixed() {
+   adjustWidth();
+   $(".child").toggleClass("fixed");
+ }
+
+ function adjustWidth() {
+   var parentwidth = $(".parent").width();
+   $(".child").width(parentwidth);
+ }
+
+ $(function() {
+
+   $("#fixer").click(
+     function() {
+       toggleFixed();
+     });
+
+   $(window).resize(
+     function() {
+       adjustWidth();
+     })
+
+ })
